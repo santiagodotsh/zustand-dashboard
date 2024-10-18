f785f9689ba6ea3162b30a3a852c06d01658f69e
Santiago Ramos <santiagodotsh@gmail.com>
2024-10-17 22:20:10 -0500

    add task and change status with immer

--- src/stores/tasks.ts
+++ src/stores/tasks.ts
@@ -1,5 +1,6 @@
 import { create } from 'zustand'
 import { devtools } from 'zustand/middleware'
+import { immer } from 'zustand/middleware/immer'
 import { v4 as uuid } from 'uuid'
 import { type Task, TaskStatus } from '../interfaces/task'
 
@@ -20,55 +21,51 @@ interface TaskState {
 
 export const useTaskStore = create<TaskState>()(
   devtools(
-    (set, get) => ({
-      draggingTaskId: undefined,
-      tasks: {
-        'ABC-1': { id: 'ABC-1', title: 'Task 1', status: TaskStatus.Pending },
-        'ABC-2': { id: 'ABC-2', title: 'Task 2', status: TaskStatus.InProgress },
-        'ABC-3': { id: 'ABC-3', title: 'Task 3', status: TaskStatus.Pending },
-        'ABC-4': { id: 'ABC-4', title: 'Task 4', status: TaskStatus.Pending },
-        'ABC-5': { id: 'ABC-5', title: 'Task 5', status: TaskStatus.Done }
-      },
+    immer(
+      (set, get) => ({
+        draggingTaskId: undefined,
+        tasks: {
+          'ABC-1': { id: 'ABC-1', title: 'Task 1', status: TaskStatus.Pending },
+          'ABC-2': { id: 'ABC-2', title: 'Task 2', status: TaskStatus.InProgress },
+          'ABC-3': { id: 'ABC-3', title: 'Task 3', status: TaskStatus.Pending },
+          'ABC-4': { id: 'ABC-4', title: 'Task 4', status: TaskStatus.Pending },
+          'ABC-5': { id: 'ABC-5', title: 'Task 5', status: TaskStatus.Done }
+        },
 
-      getTaskByStatus: (status) => Object.values(get().tasks).filter(task => task.status === status),
-      addTask: (title, status) => {
-        const newTask = {
-          id: uuid(),
-          title,
-          status
-        }
-
-        set(state => ({
-          tasks: {
-            ...state.tasks,
-            [newTask.id]: newTask
+        getTaskByStatus: (status) => Object.values(get().tasks).filter(task => task.status === status),
+        addTask: (title, status) => {
+          const newTask = {
+            id: uuid(),
+            title,
+            status
           }
-        }))
-      },
 
-      setDraggingTaskId: (taskId) => set({ draggingTaskId: taskId }),
-      removeDraggingTaskId: () => set({ draggingTaskId: undefined }),
-     
-      changeStatus: (taskId, status) => {
-        const task = get().tasks[taskId]
-        task.status = status
+          set(state => {
+            state.tasks[newTask.id] = newTask
+          })
+        },
 
-        set(state => ({
-          tasks: {
-            ...state.tasks,
-            [taskId]: task
-          }
-        }))
-      },
+        setDraggingTaskId: (taskId) => set({ draggingTaskId: taskId }),
+        removeDraggingTaskId: () => set({ draggingTaskId: undefined }),
       
-      onTaskDrop: (status) => {
-        const taskId = get().draggingTaskId
+        changeStatus: (taskId, status) => {
+          const task = { ...get().tasks[taskId] }
+          task.status = status
 
-        if (!taskId) return
+          set(state => {
+            state.tasks[taskId] = task
+          })
+        },
+        
+        onTaskDrop: (status) => {
+          const taskId = get().draggingTaskId
 
-        get().changeStatus(taskId, status)
-        get().removeDraggingTaskId()
-      }
-    })
+          if (!taskId) return
+
+          get().changeStatus(taskId, status)
+          get().removeDraggingTaskId()
+        }
+      })
+    )
   )
 )
